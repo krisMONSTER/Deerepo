462063c982869a5fb7df6d7e841a6f7512b26b6c
Krystian <xperiaevool@gmail.com>
2020-04-19 18:44:48 +0200

    removed lolo

--- src/Paczka/Evool.java
+++ src/Paczka/Evool.java
@@ -6,7 +6,7 @@ public class Evool extends Thread {
     public void run() {
         for(int i = 0; i < 100000000; i++) {
             Math.pow(2,30);
-            System.out.println("hahalolo");
+            System.out.println("haha");
             System.out.println("no cos");
         }
     }
