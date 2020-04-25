cc9ed367b6959d5f2a19ccf0b7777be208cedd63
NataliaM <61874467+xThalia@users.noreply.github.com>
2020-04-25 23:32:48 +0200

    pionek moze sie cofac na stare miejsce - poprawiony blad

--- src/GUI/Connection.java
+++ src/GUI/Connection.java
@@ -10,6 +10,7 @@ import javafx.scene.layout.GridPane;
 public class Connection {
 
     boolean is_picked; //Info dla EventHandlera czy przypadkiem nie wybrano juz jakiegos pionka
+    Label piece;
     ImageView picked_piece; //Ikona wybranego pionka
     Lighting light = new Lighting();
     Label[][] board;
@@ -24,19 +25,25 @@ public class Connection {
     public void moving_on_chessboard()
     {
         EventHandler<MouseEvent> eventHandler = e -> {
-            int ustal_kolumne;
-            int ustal_wiersz;
-            ustal_kolumne=(int)((e.getX()/50)+1);
-            ustal_wiersz=(int)((e.getY()/50+1));
+            int find_col;
+            int find_row;
+
+            find_col=(int)((e.getX()/50)+1);
+            find_row=(int)((e.getY()/50+1));
+
+            //czy juz zostal wybrany pionek, jesli nie to zaznacza go
             if(is_picked==false) {
-                if(board[ustal_wiersz-1][ustal_kolumne-1].getGraphic()==null) return;
-                picked_piece = (ImageView) board[ustal_wiersz-1][ustal_kolumne-1].getGraphic();
-                board[ustal_wiersz-1][ustal_kolumne-1].getGraphic().setEffect(light);
+                if(board[find_row-1][find_col-1].getGraphic()==null) return;
+                piece = board[find_row-1][find_col-1];
+                picked_piece = (ImageView) board[find_row-1][find_col-1].getGraphic();
+                board[find_row-1][find_col-1].getGraphic().setEffect(light);
                 is_picked = true;
             }
+            //jesli zostal wybrany wczesniej pionek to przenosi go na nowe miejsce
             else{
-                board[ustal_wiersz-1][ustal_kolumne-1].setGraphic(picked_piece);
-                board[ustal_wiersz-1][ustal_kolumne-1].getGraphic().setEffect(null);
+                piece.setGraphic(null);
+                board[find_row-1][find_col-1].setGraphic(picked_piece);
+                board[find_row-1][find_col-1].getGraphic().setEffect(null);
                 is_picked = false;
             }
         };
