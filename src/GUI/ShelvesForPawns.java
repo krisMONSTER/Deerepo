bc97eff294d8ce305e2440b1f26fdb87d914d251
NataliaM <61874467+xThalia@users.noreply.github.com>
2020-06-04 00:59:25 +0200

    przygotowania do zrobienia "półek" na zbite piony - GUI

---
+++ src/GUI/ShelvesForPawns.java
@@ -0,0 +1,11 @@
+package GUI;
+
+import javafx.scene.layout.HBox;
+
+public class ShelvesForPawns {
+    public static void addShelves()
+    {
+        HBox topshelf;
+        HBox bottomshelf;
+    }
+}
